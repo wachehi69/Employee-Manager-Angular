9f0e03fc4307cdaa24752288d20367e05514652d
wachehi69 <wachehi6905@gmail>
2022-08-04 11:52:08 +0200

    ajout du module service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { EmployeService } from './employe.service';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit{
+  
+  public employees!: Employee[];
+
+  constructor(private employeeService: EmployeService) { }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+
+    this.employeeService.getEmployees().subscribe(
+       (response: Employee[]) => {
+        this.employees = response;
+       },
+       (error: HttpErrorResponse) => {
+        alert(error.message);
+       }
+       );    
+  }
+
+
 }
